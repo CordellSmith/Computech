cc0894c6a56a450255c1a8f44745451db977bfb5
kevin-rad <kevin.nederi-rad@hotmail.com>
2016-10-25 22:08:21 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,77 +1,28 @@
 <!DOCTYPE html>
 <?php
-$servername = "localhost";
-$username = "X32237834";
-$password = "X32237834";
-$dbname = "X32237834";
-
-$connection = mysql_connect("$servername", "$username", "$password");
-mysql_select_db("$dbname");
-
-if ($db -> connect_error)
-{
-	die("Connection failed: " . $conn->connect_error);
-}
+	$servername = "localhost";
+	$username = "X32237834";
+	$password = "X32237834";
+	$dbname = "X32237834";
+	
+	$connection = mysql_connect("$servername", "$username", "$password");
+	mysql_select_db("$dbname");
+	
+	if ($db -> connect_error)
+	{
+		die("Connection failed: " . $conn->connect_error);
+	}
 ?>
 <html>
-<head>
-	<title>Registered</title>
-	<link rel="stylesheet" type="text/css" href="style1.css"/>
-</head>
-<body>
-	<div id="container">
-		<div id="header">
-			<h1>CompuTech</h1>
-			<ul>
-				<li><a href="index.html">Home</a></li>
-				<li><a href="about.html">About</a></li>
-				<li><a href="contact.html">Contact</a></li>
-				<li style="float:right"><a href="login.html">Login</a></li>
-				<li style="float:right"><a class="active" href="register.html">Register</a></li>
-			</ul>
-		</div>
-		<div id="content">
-			<div id="nav">
-				<h2>Categories</h2>
+	<head>
+		<title>Registered</title>
+		<link rel="stylesheet" type="text/css" href="style1.css"/>
+	</head>
+	<body>
+		<div id="container">
+			<div id="header">
+				<h1>CompuTech</h1>
 				<ul>
-<<<<<<< HEAD
-					<li><a href="case.html"><span class="cat_li">Cases</span><img src="logos/case.png" alt="Towers" id="cat_img"></a></li>
-					<li><a href="mboard.html"><span class="cat_li">Motherboards</span><img src="logos/mboard.png" alt="Motherboard"></a></li>
-					<li><a href="cpu.html"><span class="cat_li">CPU</span><img src="logos/cpu.png" alt="CPU"></a></li>
-					<li><a href="ram.html"><span class="cat_li">RAM</span><img src="logos/ram.png" alt="RAM"></a></li>
-					<li><a href="graphics.html"><span class="cat_li">Graphics</span><img src="logos/gpu.png" alt="Graphics"></a></li>
-					<li><a href="storage.html"><span class="cat_li">Storage</span><img src="logos/hdd.png" alt="HDD"></a></li>
-					<li><a href="monitor.html"><span class="cat_li">Monitors</span><img src="logos/monitor.png" alt="Monitors"></a></li>
-					<li><a href="software.html"><span class="cat_li">Software</span><img src="logos/software.png" alt="Software"></a></li>
-					<li><a href="accessories.html"><span class="cat_li">Accessories</span><img src="logos/accessories.png" alt="Accessories"></a></li>
-				</ul>
-			</div>
-			<div id="main">
-				<h3>Thank You<br></h3>
-				
-				<?php
-				$user = 	$_POST[ "username" ];
-				$pw = 		$_POST[ "password" ];
-				$fname = 	$_POST[ "firstname" ];
-				$lname = 	$_POST[ "lastname" ];
-				$st = 		$_POST[ "street" ];
-				$cty = 		$_POST[ "city" ];
-				$cntry = 	$_POST[ "country" ];
-				$ph = 		$_POST[ "phonenumber" ];
-				$eml = 		$_POST[ "email" ];
-				
-				mysql_query("INSERT INTO users (username, password, firstname, lastname, street, city, country, phonenumber, email) 
-					VALUES('$user', '$pw', '$fname', '$lname', '$st', '$cty', '$cntry', '$ph', '$eml')")
-				
-				or die(mysql_error());
-				
-				mysql_close($connection);
-				
-				echo("You are now registered with CompuTech!");
-				?>
-				
-				<p></p>
-=======
 					<li><a href="index.html">Home</a></li>
 					<li><a href="about.html">About</a></li>
 					<li><a href="contact.html">Contact</a></li>
@@ -84,15 +35,15 @@ if ($db -> connect_error)
 				<div id="nav">
 					<h2>Categories</h2>
 					<ul>
-						<li><a href="categories/case.php"><img src="logos/case.png" alt="Towers"></a></li>
-						<li><a href="categories/motherboard.php"><img src="logos/mboard.png" alt="Motherboard"></a></li>
-						<li><a href="categories/cpu.php"><img src="logos/cpu.png" alt="CPU"></a></li>
-						<li><a href="categories/ram.php"><img src="logos/ram.png" alt="RAM"></a></li>
-						<li><a href="categories/gpu.php"><img src="logos/gpu.png" alt="Graphics"></a></li>
-						<li><a href="categories/hdd.php"><img src="logos/hdd.png" alt="HDD"></a></li>
-						<li><a href="categories/monitor.php"><img src="logos/monitor.png" alt="Monitors"></a></li>
-						<li><a href="categories/software.php"><img src="logos/software.png" alt="Software"></a></li>
-						<li><a href="categories/accessories.php"><img src="logos/accessories.png" alt="Accessories"></a></li>
+						<li><a href="categories/case.php"><span class="cat_li">Cases</span><img src="logos/case.png" alt="Towers"></a></li>
+						<li><a href="categories/motherboard.php"><span class="cat_li">Motherboard</span><img src="logos/mboard.png" alt="Motherboard"></a></li>
+						<li><a href="categories/cpu.php"><span class="cat_li">CPU</span><img src="logos/cpu.png" alt="CPU"></a></li>
+						<li><a href="categories/ram.php"><span class="cat_li">RAM</span><img src="logos/ram.png" alt="RAM"></a></li>
+						<li><a href="categories/gpu.php"><span class="cat_li">GPU</span><img src="logos/gpu.png" alt="Graphics"></a></li>
+						<li><a href="categories/hdd.php"><span class="cat_li">STORAGE</span><img src="logos/hdd.png" alt="HDD"></a></li>
+						<li><a href="categories/monitor.php"><span class="cat_li">DISPLAY</span><img src="logos/monitor.png" alt="Monitors"></a></li>
+						<li><a href="categories/software.php"><span class="cat_li">SOFTWARE</span><img src="logos/software.png" alt="Software"></a></li>
+						<li><a href="categories/accessories.php"><span class="cat_li">ACCESSORIES</span><img src="logos/accessories.png" alt="Accessories"></a></li>
 					</ul>
 				</div>
 				<div id="main">
@@ -120,13 +71,12 @@ if ($db -> connect_error)
 					?>
 					
 					<p></p>
->>>>>>> origin/master
 				<p></p>
+				</div>
+			</div>
+			<div id="footer">
+				Copyright &copy; 2016. computech inc.
 			</div>
 		</div>
-		<div id="footer">
-			Copyright &copy; 2016. computech inc.
-		</div>
-	</div>
 </body>
 </html>
