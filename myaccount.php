4a417fe1855e99af56b2fe97b42523f3d623c000
kevin-rad <kevin.nederi-rad@hotmail.com>
2016-11-06 15:18:16 +0800

    Update myaccount.php

--- myaccount.php
+++ myaccount.php
@@ -5,18 +5,19 @@
 	$password = "X32237834";
 	$dbname = "X32237834";
 	
-	$connection = mysql_connect("$servername", "$username", "$password");
-	mysql_select_db("$dbname");
+	$conn = mysqli_connect("$servername", "$username", "$password");
+	mysqli_select_db($conn, "$dbname");
 	
-	if ($db -> connect_error)
+	if (!$conn)
 	{
-		die("Connection failed: " . $conn->connect_error);
+		die("Connection failed: " . $conn -> connect_error);
 	}
+	include('session.php');
 ?>
 <html>
 	<head>
-		<title>CompuTech MyAccount</title>
-		<link rel="stylesheet" type="text/css" href="style1.css"/>
+		<title>MyAccount</title>
+		<link rel="stylesheet" type="text/css" href="register_login_style.css"/>
 	</head>
 	<body>
 		<div id="container">
@@ -26,40 +27,74 @@
 					<li><a href="index.html">Home</a></li>
 					<li><a href="about.html">About</a></li>
 					<li><a href="contact.html">Contact</a></li>
+					<li><a href="all_products.php">Products</a></li>
 					<li><a href="search.php">Search</a></li>
-					<li style="float:right"><a href="login.html">Login</a></li>
-					<li style="float:right"><a class="active" href="register.html">Register</a></li>
+					<li style="float:right"><a class="active" href="myaccount.php">MyAccount</a></li>
+					<li style="float:right"><a href="register.html">Register</a></li>
 				</ul>
 			</div>
 			<div id="content">
 				<div id="nav">
 					<h2>Categories</h2>
-					<ul>
-						<li><a href="categories/case.php"><span class="cat_li">CASES</span><img src="logos/case.png" alt="Towers"></a></li>
-						<li><a href="categories/motherboard.php"><span class="cat_li">MOTHERBOARDS</span><img src="logos/mboard.png" alt="Motherboard"></a></li>
-						<li><a href="categories/cpu.php"><span class="cat_li">CPU</span><img src="logos/cpu.png" alt="CPU"></a></li>
-						<li><a href="categories/ram.php"><span class="cat_li">RAM</span><img src="logos/ram.png" alt="RAM"></a></li>
-						<li><a href="categories/gpu.php"><span class="cat_li">GPU</span><img src="logos/gpu.png" alt="Graphics"></a></li>
-						<li><a href="categories/hdd.php"><span class="cat_li">STORAGE</span><img src="logos/hdd.png" alt="HDD"></a></li>
-						<li><a href="categories/monitor.php"><span class="cat_li">DISPLAY</span><img src="logos/monitor.png" alt="Monitors"></a></li>
-						<li><a href="categories/software.php"><span class="cat_li">SOFTWARE</span><img src="logos/software.png" alt="Software"></a></li>
-						<li><a href="categories/accessories.php"><span class="cat_li">ACCESSORIES</span><img src="logos/accessories.png" alt="Accessories"></a></li>
+					<ul class="img-list">
+						<li><a href="categories/case.php">
+							<span class="cat_li">TOWERS</span>
+							<img src="logos/case.png" alt="Towers"></a>
+						</li>
+						<li><a href="categories/motherboard.php">
+							<span class="cat_li">MOTHERBOARD</span>
+							<img src="logos/mboard.png" alt="Motherboard"></a>
+						</li>
+						<li><a href="categories/cpu.php">
+							<span class="cat_li">CPU</span>
+							<img src="logos/cpu.png" alt="CPU"></a>
+						</li>
+						<li><a href="categories/ram.php">
+							<span class="cat_li">RAM</span>
+							<img src="logos/ram.png" alt="RAM"></a>
+						</li>
+						<li><a href="categories/gpu.php">
+							<span class="cat_li">GPU</span>
+							<img src="logos/gpu.png" alt="Graphics"></a>
+						</li>
+						<li><a href="categories/hdd.php">
+							<span class="cat_li">STORAGE</span>
+							<img src="logos/hdd.png" alt="HDD"></a>
+						</li>
+						<li><a href="categories/monitor.php">
+							<span class="cat_li">DISPLAY</span>
+							<img src="logos/monitor.png" alt="Monitors"></a>
+						</li>
+						<li><a href="categories/software.php">
+							<span class="cat_li">SOFTWARE</span>
+							<img src="logos/software.png" alt="Software"></a>
+						</li>
+						<li><a href="categories/accessories.php">
+							<span class="cat_li">ACCESSORIES</span>
+							<img src="logos/accessories.png" alt="Accessories"></a>
+						</li>
 					</ul>
 				</div>
 				<div id="main">
 					<h3>My Account<br></h3>
+					<h2><a href = "logout.php">Sign Out</a></h2>
 					
 					<?php
-						$user = 	$_POST[ "username" ];
-						$pw = 		$_POST[ "password" ];
-			
-						mysql_query("")
-						
-						or die(mysql_error());
+							$user = $_SESSION['login_user'];
+							
+							
+							$sql = "SELECT username FROM users WHERE username='$user'";
+							$result = mysqli_query($conn, $sql);
+
+							$user_details_array = array();
+
+							while($row = mysqli_fetch_array($result))
+							{
+								$user_details_array[] = "\"".$row['username']."\"";
+							}
+							
+							$username_string = implode(",", $user_details_array);
 					?>
-					
-					<p></p>
-				<p></p>
 				</div>
 			</div>
 			<div id="footer">
