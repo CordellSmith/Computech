03f339fce22e92d920f67179d9ed084fcdb313e4
kevin-rad <kevin.nederi-rad@hotmail.com>
2016-11-08 03:16:43 +0800

    Update login.php

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
 		$myusername = mysqli_real_escape_string($conn,$_POST['username']);
 		$mypassword = mysqli_real_escape_string($conn,$_POST['password']); 
 
-		$sql = "SELECT firstname FROM users WHERE username='$myusername' and password='$mypassword'";
+		$sql = "SELECT username FROM users WHERE username='$myusername' and password='$mypassword'";
 		$result = mysqli_query($conn,$sql);
 		$row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 		$active = $row['active'];
